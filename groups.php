1e70d3af0d9980f5c69eb373509b8d9eb8c1ed91
Prasetyono Putra <prasetyonoptr@gmail.com>
2024-02-09 22:31:23 +0700

    Fix Bug

--- groups.php
+++ groups.php
@@ -2,15 +2,15 @@
 session_start();
 include_once "php/config.php";
 if (!isset($_SESSION['unique_id'])) {
-  header("location: login.php");
+    header("location: login.php");
 }
 
 ?>
 
 <?php if (isset($SESSION['message'])) { ?>
-<script>
-alert('<?= $SESSION['message'] ?>');
-</script>
+    <script>
+        alert('<?= $SESSION['message'] ?>');
+    </script>
 <?php } ?>
 
 <?php include_once "header.php"; ?>
@@ -20,20 +20,24 @@ alert('<?= $SESSION['message'] ?>');
         <section class="users">
             <header>
                 <div class="content">
-                    <?php 
-            $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
-            if(mysqli_num_rows($sql) > 0){
-              $row = mysqli_fetch_assoc($sql);
-            }
-          ?>
+                    <?php
+                    $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+                    if (mysqli_num_rows($sql) > 0) {
+                        $row = mysqli_fetch_assoc($sql);
+                    }
+                    ?>
                     <button type="button" class="" style="border: none; background: transparent; cursor:pointer;"
                         data-bs-toggle="modal" data-bs-target="#exampleModal"
                         onclick="openModalProfile('<?= $row['unique_id'] ?>')">
                         <img src="php/images/<?php echo $row['img']; ?>" alt="">
                     </button>
                     <div class="details">
-                        <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
-                        <p style="color: #000;"><?php echo $row['status']; ?></p>
+                        <span>
+                            <?php echo $row['fname'] . " " . $row['lname'] ?>
+                        </span>
+                        <p style="color: #000;">
+                            <?php echo $row['status']; ?>
+                        </p>
                     </div>
                 </div>
 
@@ -61,15 +65,15 @@ alert('<?= $SESSION['message'] ?>');
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+        </script>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
-    </script>
+        </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
         integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
-    </script>
+        </script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
 
--- php/group.php
+++ php/group.php
@@ -13,23 +13,16 @@ if (mysqli_num_rows($query) == 0) {
     $output .= "Tidak ada grup yang tersedia";
 } elseif (mysqli_num_rows($query) > 0) {
     while ($row = mysqli_fetch_assoc($query)) {
-        $sql2 = "SELECT * FROM message_group";
+        $sql2 = "SELECT * FROM message_group WHERE id_group='{$row['id']}' ORDER BY timestamp DESC LIMIT 1";
         $query2 = mysqli_query($conn, $sql2);
         $row2 = mysqli_fetch_assoc($query2);
-        (mysqli_num_rows($query2) > 0) ? $result = $row2['message'] : $result = "Tidak ada pesan tersedia";
-        (strlen($result) > 28) ? $msg = substr($result, 0, 28) . '...' : $msg = $result;
-        if (isset($row2['outgoing_msg_id'])) {
-            ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "Anda: " : $you = "";
-        } else {
-            $you = "";
-        }
 
         $output .= '<a href="chatGroup.php?idGroup=' . $row['id'] . '">
                         <div class="content">
                         <img src="php/images/' . $row['img'] . '" alt="">
                         <div class="details">
                             <span>' . $row['name_group'] . '</span>
-                            <p>' . $you . $msg . '</p>
+                            <p>' . $row2['message'] . '</p>
                         </div>
                         </div>
                     </a>';
