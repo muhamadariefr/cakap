db1809312fbfd17876fcd1f0ffdb8baa15cbe032
Prasetyono Putra <prasetyonoptr@gmail.com>
2024-02-28 09:55:49 +0700

    Edit group

---
+++ editGroup.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+include_once "php/config.php";
+?>
+<?php include_once "header.php"; ?>
+
+<body>
+    <div class="loading-screen">
+        <img class="loading-icon" src="chat.png" alt="Chat Icon">
+        <p>Cakap Messenger</p>
+    </div>
+    <div class="wrapper">
+        <section class="form signup">
+            <header class="title-log-reg">Cakap Messenger
+            </header>
+
+            <?php
+            $idGroup = mysqli_real_escape_string($conn, $_GET['idGroup']);
+            $sql = mysqli_query($conn, "SELECT * FROM tbl_groups WHERE id = {$idGroup}");
+            if (mysqli_num_rows($sql) > 0) {
+                $row = mysqli_fetch_assoc($sql);
+            } else {
+                header("location: users.php");
+            }
+            ?>
+
+            <form id="form-editGroup" enctype="multipart/form-data" autocomplete="off">
+                <div class="field input">
+                    <img src="php/images/<?php echo $row['img'] ?>" alt="">
+                    <label>Nama Group</label>
+                    <input type="text" name="nameGroup" placeholder="Masukan nama grup" value="<?php echo $row['name_group'] ?>" required>
+                    <input type="hidden" name="idGroup" value="<?php echo $_GET['idGroup']; ?>">
+                </div>
+                <div class="field image">
+                    <label>Pilih Gambar</label>
+                    <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg">
+                </div>
+                <div class="field button">
+                    <input type="submit" name="submit" value="Edit Group">
+                </div>
+            </form>
+        </section>
+        <footer>
+            <p>&#169; 2024 Cakap Messenger</p>
+        </footer>
+    </div>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            // Tampilkan layar loading
+            const loadingScreen = document.querySelector('.loading-screen');
+            loadingScreen.style.display = 'flex'; // Gunakan 'flex' agar ikon berada di tengah
+
+            // Sembunyikan halaman registrasi
+            const registrationPage = document.querySelector('.wrapper');
+            registrationPage.style.display = 'none';
+
+            // Tunda tampilan halaman registrasi selama 1000ms (1 detik)
+            setTimeout(function () {
+                // Sembunyikan layar loading
+                loadingScreen.style.display = 'none';
+
+                // Tampilkan halaman registrasi
+                registrationPage.style.display = 'block';
+            }, 2000);
+        });
+
+        const form = document.getElementById("form-editGroup");
+
+        form.onsubmit = function (e) {
+            e.preventDefault();
+
+            const formData = new FormData(form);
+
+            fetch('php/editGroup.php', {
+                method: 'POST',
+                body: formData,
+            })
+                .then(response => response.json())
+                .then(data => {
+                    alert(data.message);
+                    window.location.href = './groups.php';
+                })
+                .catch(error => {
+                    alert(data.message);
+                });
+        };
+    </script>
+
+</body>
+
+</html>
---
+++ php/editGroup.php
@@ -0,0 +1,95 @@
+<?php
+include_once "config.php";
+
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nameGroup = isset($_POST['nameGroup']) ? $_POST['nameGroup'] : '';
+    $idGroup = isset($_POST['idGroup']) ? $_POST['idGroup'] : '';
+    $img_name = '';
+
+    if (isset($_FILES['image'])) {
+        $img_name = uploadImage();
+    }
+
+    $response = editGroup($nameGroup, $img_name, $idGroup);
+
+    echo json_encode($response);
+}
+
+function editGroup($name, $image, $idGroup)
+{
+    global $conn;
+
+    try {
+        http_response_code(201);
+
+
+
+        if ($image == '') {
+            $sql = "
+        UPDATE tbl_groups
+        SET
+        name_group = '$name'
+        WHERE id = '$idGroup'
+        ";
+
+            $query = mysqli_query($conn, $sql);
+        } else {
+            $sql = "
+        UPDATE tbl_groups
+        SET
+        name_group = '$name',
+        img = '$image'
+        WHERE id = '$idGroup'
+        ";
+
+            $query = mysqli_query($conn, $sql);
+        }
+
+
+
+        if ($query) {
+            return [
+                'status'    => 'success',
+                'message'   => 'Grup berhasil dirubah',
+                'timestamp' => time()
+            ];
+        } else {
+            return [
+                'status'  => 'error',
+                'message' => 'Terjadi kesalahan: ' . $e->getMessage()
+            ];
+        }
+    } catch (Exception $e) {
+        return [
+            'status'  => 'error',
+            'message' => 'Terjadi kesalahan: ' . $e->getMessage()
+        ];
+    }
+}
+
+
+function uploadImage()
+{
+    $img_name = $_FILES['image']['name'];
+    $img_type = $_FILES['image']['type'];
+    $tmp_name = $_FILES['image']['tmp_name'];
+
+    $img_explode = explode('.', $img_name);
+    $img_ext = strtolower(end($img_explode));
+
+    $allowed_extensions = ["jpeg", "png", "jpg"];
+    $allowed_types = ["image/jpeg", "image/jpg", "image/png"];
+
+    if (in_array($img_ext, $allowed_extensions) && in_array($img_type, $allowed_types)) {
+        $new_img_name = time() . $img_name;
+        $destination_path = "images/" . $new_img_name;
+
+        if (move_uploaded_file($tmp_name, $destination_path)) {
+            return $new_img_name;
+        }
+    }
+
+    return "default.png";
+}
